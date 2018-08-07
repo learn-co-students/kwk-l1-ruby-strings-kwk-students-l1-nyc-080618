658dadd620bcd467c01896696d79f4ffc57e0eee
Sarah Ng <agbtr101a@gmail.com>
2018-08-07 15:59:49 +0000

    Done.

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,12 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
+def say_hello(guest_name)
+puts "Hello (guest_name)"
+puts "party_name"
+puts "date"
+puts "time"
+puts "host_name"
+end
+say_hello("Hermoine")
+say_hello("Luna")
